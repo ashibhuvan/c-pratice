913f47d16a8a3f518970d0773d2a5810021c819a
Mehta <k1630620@nmscde004200.nms.kcl.ac.uk>
2017-10-17 18:36:57 +0100

    this is the actual first commit

---
+++ main.c
@@ -0,0 +1,25 @@
+/* this is a comment in c */
+#include <iostream>
+using std::vector
+
+
+
+int main(){
+	
+
+	vector<int> name_of_stocks(25);
+	for(int i=0; i<10; ++i){
+		name_of_stocks		
+	} 
+
+	std::cout << "hello world how are you\n";
+	return 0;
+	
+
+
+}
+
+
+
+
+
